8104e0bce244114757afac3db47cdd1bcf0c1bd9
malvin <malvin@xtramilesolutions.com.au>
2022-08-18 00:23:23 +0700

    Initial version of returnkeyapp generated by generator-jhipster@7.5.0

---
+++ src/main/java/com/rkey/returnkeyapp/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.rkey.returnkeyapp.security;
---
+++ src/test/java/com/rkey/returnkeyapp/ArchTest.java
@@ -0,0 +1,29 @@
+package com.rkey.returnkeyapp;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.rkey.returnkeyapp");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.rkey.returnkeyapp.service..")
+            .or()
+            .resideInAnyPackage("com.rkey.returnkeyapp.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.rkey.returnkeyapp.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
